98702a508e94dde2c0aee10f0b52da8db852d78e
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-07-27 10:39:03 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+//Scanner를 사용하기 위해 Scanner클래스를 import
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // System.in은 입력한 값을 byte 단위로 읽는 것
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        // a 와 b 를 입력한다. a 와 b 는 정수형이므로 nextInt 사용
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // 입력된 a 와 b 를 더하여 출력한다.
+        System.out.println(a + b);
+    }
+}
