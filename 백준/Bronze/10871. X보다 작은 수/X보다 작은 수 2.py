46d9349055c0fad9ac79330a133e4cbeb14d5a3c
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-07-30 15:49:03 +0900

    Update X보다 작은 수 2.py

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수 2.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수 2.py
@@ -1,9 +1,6 @@
-# 입력받은 값을 공백을 기준으로 분리하고, 각각 정수로 변환한 뒤 변수에 할당
-n, x = map(int, input().split()) 
+N, X = map(int, input().split())
 
-# 사용자로부터 N개의 정수를 입력받아 n_list에 리스트 형태로 저장
-n_list = list(map(int, input().split(" ")))
-
-for i in n_list: # 리스트 n_list에 담긴 각각의 정수에 대해 아래의 동작을 반복
-    if i < x:  # 만약 현재의 정수 i가 x보다 작다면, 이를 출력
-        print(i, end=" ")  # 출력 후 줄바꿈 대신 공백을 출력
+# List Comprehension 을 이용하여 입력받은 수열에서 X보다 작은 수를 찾아 새로운 리스트 arr에 저장
+# 문자열을 공백 기준으로 나눈 리스트를 반환, 각 원소는 int(x)를 통해 정수로 변환
+arr = [x for x in input().split() if int(x) < X] # 리스트에 저장할 때부터 값을 비교해서 작은 값만 저장
+print(" ".join(arr))  # join : 리스트의 요소들을 공백으로 이어 붙여 문자열로 변환하고 출력
