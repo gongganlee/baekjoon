8624b1b94656f99847e24d3f018257258324bc94
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-08 03:45:07 +0900

    Update A＋B.java

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,17 +1,12 @@
-//Scanner클래스를 import
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // System.in : byte 단위로 읽는 것
-        Scanner sc = new Scanner(System.in);
-        int a, b;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 
-        // a 와 b 를 입력한다. a 와 b 는 정수형이므로 nextInt 사용
-        a = sc.nextInt();
-        b = sc.nextInt();
-
-        // 입력된 a 와 b 를 더하여 출력한다.
-        System.out.println(a + b);
-    }
+		System.out.println(a + b);
+	}
 }
