547f0051281d5d77dd34a00fdb5168599853a2a8
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-10 20:08:54 +0900

    Create A＋B － 3 2.java

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int i = 0;
+        while(i < T){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+            i++;
+        }
+    }
+}
