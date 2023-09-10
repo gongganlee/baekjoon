db1a53af8b3ea733ccde82b475522f4fa0cac362
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-10 16:11:39 +0900

    Update 사분면 고르기.java

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -8,7 +8,7 @@ public class Main {
         int y = sc.nextInt();
         if (x>0 && y>0)
             System.out.println("1");
-        else if(x<0 && y>0)
+        else if(x>0 && y<0)
             System.out.println("2");
         else if(x<0 && y<0)
             System.out.println("3");
