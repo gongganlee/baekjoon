dd8f651ba51771a3a6ac36a229ce73efb395e1d7
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-11 09:18:15 +0900

    [Bronze V] Title: 합, Time: 200 ms, Memory: 17664 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -6,10 +6,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int sum = 0;
-        
-        for (int i = 1; i < n; i++){
+
+        for (int i = 1; i <= n; i++){
         sum += i;
         }
-        System.out.println(n+sum);
+        System.out.println(sum);
     }
 }
