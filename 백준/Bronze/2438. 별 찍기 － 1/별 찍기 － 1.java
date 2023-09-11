01e4908c9c3f67233613e46c16c30088c29bde39
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-11 11:21:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int x = 1; x <= N; x++) { //1~N행
+            for (int y = 1; y <= x; y++) { //별의 개수
+                System.out.print("*"); //별 찍고
+            }
+            System.out.println(); //개행
+        }
+    }
+}
