daf9a0be0f0af0659fc4a6f178a06e98109d9d3d
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-12 09:24:44 +0900

    Create A+B -7 2.java

---
+++ 백준/Bronze/11021. A＋B － 7/A+B -7 2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 1; i <= T; i++) {
+            int A = sc.nextInt(), B = sc.nextInt();
+            System.out.printf("Case #%s: %s \n", i, A+B);
+        }
+    }
+}
+
+// printf 사용. 개행되지 않으므로 \n을 추가로 넣어주었다. 
