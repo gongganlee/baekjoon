7ccdab76ffdb0650b979ace8bc5b87944a975933
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-15 14:43:37 +0900

    Update A－B.java

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+    public static void main(String[] args) {
 
-		System.out.println(a - b);
-	}
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a - b);
+
+        sc.close();
+    }
 }
