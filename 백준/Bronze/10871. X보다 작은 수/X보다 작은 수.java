9f6adf2fa7cbae477e98c93f95e2c39971d340bb
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-17 19:33:45 +0900

    [Bronze V] Title: X보다 작은 수, Time: 676 ms, Memory: 31564 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -5,12 +5,11 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt(), X=sc.nextInt();
-        int [] num = new int[N]; // 정수 N개로 이루어진 수열 A
 
         for(int i=0; i<N; i++){
-            num[i] = sc.nextInt(); //X보다 작은 수를 저장
-            if( num[i] < X)
-                System.out.print(num[i] + " ");
+            int a = sc.nextInt();
+            if(a < X)
+                System.out.print(a + " ");
         }
     }
 }
