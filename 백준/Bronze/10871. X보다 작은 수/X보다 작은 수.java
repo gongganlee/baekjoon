726086cc3341b690c2ce229fbcc617fb2bb5bf56
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-17 19:29:47 +0900

    [Bronze V] Title: X보다 작은 수, Time: 716 ms, Memory: 31240 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(), X=sc.nextInt();
+        int [] num = new int[N]; // 정수 N개로 이루어진 수열 A
+
+        for(int i=0; i<N; i++){
+            num[i] = sc.nextInt(); //X보다 작은 수를 저장
+            if( num[i] < X)
+                System.out.print(num[i] + " ");
+        }
+    }
+}
