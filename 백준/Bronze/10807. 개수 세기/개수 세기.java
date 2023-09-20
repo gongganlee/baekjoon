0e829333f79c22bc0a2d70697b5cb3dfff63b503
space_lee <109786433+gongganlee@users.noreply.github.com>
2023-09-20 19:16:34 +0900

    Update 개수 세기.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); //정수의 개수 N (1<=N<=100)
+        int N = sc.nextInt();
         int [] num = new int[N]; // 입력받은 수(N)만큼 index 생성
 
         for(int i=0; i<num.length; i++){
-            num[i] = sc.nextInt(); // 입력받은 수(N)만큼 arr에 숫자를 입력받아 저장
+            num[i] = sc.nextInt(); // 입력받은 수(N)만큼 숫자를 입력받아 저장
         }
 
-        int v = sc.nextInt(); // 찾을 숫자
+        int v = sc.nextInt();
         sc.close();
-        int count = 0; // 찾을 숫자의 개수
+        int count = 0;
 
         for (int i=0; i<num.length; i++) {
             if (num[i] == v) {
