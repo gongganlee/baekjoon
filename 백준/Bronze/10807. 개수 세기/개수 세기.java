ef9153a888b06cd8ca707d976390dcb1d114a6bb
sunforest <109786433+leesunforest@users.noreply.github.com>
2023-11-01 01:48:42 +0000

    Update 개수 세기.java

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -5,10 +5,10 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int [] num = new int[N]; // 입력받은 수(N)만큼 index 생성
+        int [] num = new int[N]; // 입력받은 수(N)만큼 index 생성 
 
         for(int i=0; i<num.length; i++){
-            num[i] = sc.nextInt(); // 입력받은 수(N)만큼 숫자를 입력받아 저장
+            num[i] = sc.nextInt(); // 입력받은 수(N)만큼 숫자를 입력받아 저장 
         }
 
         int v = sc.nextInt();
