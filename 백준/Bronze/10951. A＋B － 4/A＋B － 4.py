404c38972bfb3da6e9173fcfdb7fa95b081799c0
sunforest <109786433+leesunforest@users.noreply.github.com>
2023-11-04 10:02:46 +0000

    Update A＋B － 4.py

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -3,7 +3,7 @@ import sys
 while True:
     try:
         a, b = map(int, sys.stdin.readline().split())
-    except ValueError:  # 입력을 하지 않을 때 예외처리
+    except ValueError:  # 입력을 하지 않을 때 예외처리 
         break
     else:
         print(a + b)
