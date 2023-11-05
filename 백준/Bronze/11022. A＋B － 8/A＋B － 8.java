af1660ffac3f073b5980ab50449fcd5c23b6ba2d
sunforest <109786433+leesunforest@users.noreply.github.com>
2023-11-05 15:20:10 +0000

    Update A＋B － 8.java

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-
         for (int i = 1; i <= T; i++) {
             int A = sc.nextInt(), B = sc.nextInt();
             System.out.println("Case #" + i + ": " + A + " + " + B + " = " + (A+B));
