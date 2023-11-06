3f3d3a307f19a5520e78079c7a1f38fbab03abd1
sunforest <109786433+leesunforest@users.noreply.github.com>
2023-11-06 05:40:36 +0000

    Update 별 찍기 － 2.java

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -6,9 +6,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) { //1~N행
-            for (int j = 0; j < n-i-1; j++) { //공백의 개수
-                System.out.print(" "); //공백 찍고
+        for (int i = 0; i < n; i++) { // 1~N행
+            for (int j = 0; j < n-i-1; j++) { // 공백의 개수
+                System.out.print(" "); // 공백 찍고
             }
             for (int k = 0; k <= i; k++) { //별의 개수
                 System.out.print("*"); //별 찍고
