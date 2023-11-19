f026a50fecd11334a866d5c3705cedb70a44fa99
sunforest <109786433+leesunforest@users.noreply.github.com>
2023-11-19 19:04:03 +0900

    Update 최댓값 2.py

--- 백준/Bronze/2562. 최댓값/최댓값 2.py
+++ 백준/Bronze/2562. 최댓값/최댓값 2.py
@@ -1,4 +1,4 @@
-num = []  # 빈 리스트 num을 생성. 입력받은 숫자들이 저장될 예정
+num = []  # 빈 리스트 num을 생성. 입력받은 숫자들이 저장될 예정 
 for i in range(9):  # for 반복문을 이용하여 9번 반복을 수행
     num.append(int(input()))  # append 함수를 이용해 끝에 하나씩 추가
 print(max(num))  # 최댓값을 출력.
